338526ef97772c6ebb92b2a304b41c8d78d65515
Antony <a2020gk@outlook.com>
2024-10-06 15:35:43 +0300

    Updated script.js: modified station names, thread colors, and ball animation; refactored threadArrow function and added stroke to ball

--- script.js
+++ script.js
@@ -11,13 +11,14 @@ const stations = {
     center: "Центр",
     residential_complex: "Жилой комплекс",
     office: "Офис",
-    stadium: "Стадион",
-    airport: "Аэропорт"
+    sports: "Спортивная",
+    interfluve: "Междуречье",
+    airport: "Аэропорт",
 }
 
 const threads = {
     red: ["station", "beach", "park", "bay", "center", "residential_complex"],
-    blue: ["station", "office", "stadium", "airport"]
+    blue: ["station", "office", "sports", "interfluve", "airport"]
 }
 
 const station = urlParams.get("station");
@@ -33,7 +34,7 @@ const curStMap = {
     blue: "red"
 }
 
-const ball = { x: 50, y: 50 };
+const ball = { x: 50, y: 50, color:0 };
 
 function render() {
 
@@ -41,8 +42,11 @@ function render() {
 
     ctx.beginPath();
     ctx.arc(ball.x, ball.y, 50, 0, Math.PI * 2);
-    ctx.fillStyle = "rgba(0,255,0,0.2)";
+    ctx.fillStyle = `HSLA(${ball.color},100%,50%,0.2)`;
+    ctx.lineWidth=1;
+    ctx.strokeStyle=`rgba(0,0,0,0.2)`;
     ctx.fill();
+    ctx.stroke();
 
     // -- Static info --
     // Splitter
@@ -54,71 +58,76 @@ function render() {
     ctx.lineWidth = 5;
     ctx.stroke();
 
-    function threadArrow(x, color, rotated) {
-        let ymin = rotated ? canvas.height - 20 : 20;
-        let ymax = rotated ? 20 : canvas.height - 20;
+    function threadArrow(rotated) {
 
-        let arrowCof = 10;
+        const x = rotated ? canvas.width - 30 : 30;
 
         ctx.beginPath();
-        ctx.moveTo(x, ymin);
-        ctx.lineTo(x, ymax);
+        ctx.moveTo(x, 20);
+        ctx.lineTo(x, canvas.height - 20);
         ctx.lineCap = "round";
-        ctx.strokeStyle = color;
         ctx.lineWidth = 5;
+        ctx.strokeStyle = thread;
         ctx.stroke();
 
+        // Arrow
         ctx.beginPath();
-        ctx.moveTo(x - 10, rotated ? ymin - 5 : ymin + 5);
-        ctx.lineTo(x + 10, rotated ? ymin - 5 : ymin + 5);
-        ctx.lineTo(x, rotated ? ymin + arrowCof : ymin - arrowCof);
+        let yb = rotated ? 15 : canvas.height - 15;
+        let ycof = rotated ? -15 : 15;
+        ctx.moveTo(x, yb);
+        ctx.lineTo(x - 10, yb - ycof);
+        ctx.lineTo(x + 10, yb - ycof);
         ctx.closePath();
-        ctx.fillStyle = color;
+        ctx.fillStyle = thread;
         ctx.fill();
 
+        const ymax = rotated ? canvas.height - 15 : canvas.height - 30;
+        const ymin = rotated ? ymax - (canvas.height - 45) : 20;
+
+        let step = (ymax - ymin) / rThread.length;
+
+        ctx.lineWidth = 5;
+        ctx.lineCap = "round";
         ctx.font = "20px Comic Sans MS, Comic Sans, cursive";
-
-        ymax = canvas.height - 30;
-        ymin = 20;
-
-        const step = (ymax - ymin) / rThread.length;
-
-        ctx.textBaseline = "middle";
-        ctx.textAlign = rotated ? "left" : "right";
+        ctx.textAlign=rotated?"right":"left";
+        ctx.textBaseline="middle";
 
         for (let y = 0; y < rThread.length; y++) {
-            let yc = y * step +step+30;
-            let yr = rotated ? canvas.height - yc : yc;
+
+            let ycord = ymin + step * y + (rotated ? step - 5 : 0);
 
             ctx.beginPath();
-            ctx.moveTo(x - 10, yr);
-            ctx.lineTo(x + 10, yr);
-            ctx.stroke();
+            ctx.moveTo(x - 10, ycord);
+            ctx.lineTo(x + 10, ycord);
 
-            if (station == rThread[y]) {
-                ctx.fillStyle = curStMap[thread];
+            if(rThread[y] == station) {
+                ctx.fillStyle=curStMap[thread];
             } else {
-                ctx.fillStyle = thread;
+                ctx.fillStyle=thread;
             }
-            ctx.fillText(stations[rThread[y]], rotated ? x + 20 : x - 20, yr);
+            ctx.fillText(stations[rThread[y]],rotated?x-20:x+20,ycord);
+
+            ctx.stroke();
         }
+
     }
-
-    threadArrow(30, thread, true)
-    threadArrow(canvas.width - 30, thread, false);
-
+    threadArrow(false);
+    threadArrow(true);
 
 }
 
 const randomFloat = (min, max) => Math.random() * (max - min) + min;
 
-let xCof = randomFloat(0.5,2);
-let yCof = randomFloat(0.5,2);
+let xCof = randomFloat(0.5, 2);
+let yCof = randomFloat(0.5, 2);
 
 function app() {
 
     ball.x += xCof;
     ball.y += yCof;
+
+    ball.color++;
+    if(ball.color>=360) ball.color=0;
 
     if (ball.x >= canvas.width - 50) {
         xCof = -randomFloat(0.5, 2);
